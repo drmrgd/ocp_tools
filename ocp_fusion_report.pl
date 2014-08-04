8af400a3337e3e5742c4f909e343e6c1f8f20013
Dave Sims <misc@lamneth.net>
2014-08-04 10:05:25 -0400

    Updated formatted and fixed some bugs

--- ocp_fusion_report.pl
+++ ocp_fusion_report.pl
@@ -6,12 +6,12 @@
 use warnings;
 use strict;
 
-use Getopt::Long;
+use Getopt::Long qw( :config bundling auto_abbrev no_ignore_case );
 use File::Basename;
 use Data::Dump;
 
 my $scriptname = basename($0);
-my $version = "v0.4.1_061614";
+my $version = "v0.5.0_080414";
 my $description = <<"EOT";
 Print out a summary table of fusions detected by the OCP Fusion Workflow VCF files. Can choose to output
 anything seen, or just limit to annotated fusions.
@@ -20,8 +20,8 @@ EOT
 my $usage = <<"EOT";
 USAGE: $scriptname [options] <vcf_file(s)>
     -r, --ref       Include reference variants too (DEFAULT = off)
-    -n, --novel     Include 'Novel' fusions in the output
-    -o, --output    Write output to file <default is STDOUT>
+    -n, --novel     Include 'Novel' fusions in the output (DEFAULT = off)
+    -o, --output    Write output to file <default =  STDOUT>
     -v, --version   Display version information
     -h, --help      Display this help text
 EOT
@@ -33,11 +33,11 @@ my $outfile;
 my $ref_calls;
 
 GetOptions( 
-    "ref|r"      => \$ref_calls,
-    "novel"      => \$novel,
-    "output=s"   => \$outfile,
-    "help"       => \$help,
-    "version"    => \$ver_info,
+    "ref|r"        => \$ref_calls,
+    "novel|n"      => \$novel,
+    "output|o=s"   => \$outfile,
+    "help|h"       => \$help,
+    "version|v"    => \$ver_info,
 );
 
 sub help { 
@@ -53,6 +53,13 @@ sub version_info {
 help if $help;
 version_info if $ver_info;
 
+# Check we have some files to process
+if ( @ARGV < 1 ) {
+    print "ERROR: You must load at least one VCF file\n";
+    print $usage;
+    exit 1;
+}
+
 # Set up output
 my $out_fh;
 if ( $outfile ) {
@@ -66,7 +73,7 @@ my @files = @ARGV;
 #######===========================  END ARG Parsing  #######=========================== 
 
 my %results;
-my @fusions;
+my $fwidth=0;
 for my $input_file ( @files ) {
     (my $name = $input_file) =~ s/(:?_Fusion_filtered)?\.vcf$//i;
     $name =~ s/_RNA//;
@@ -82,6 +89,7 @@ for my $input_file ( @files ) {
             # TODO: Not sure this is accurate.  how are novel fusions reported.  Are those without IDs novel?
             #( grep { /NOVEL/ } @data ) ? ($annot = "NOVEL") : ( ($annot) = map { /ANNOTATION=(.*?);FUNC/ } @data );
             ( ! grep { /ANNOTATION/ } @data ) ? ($annot = "NOVEL") : ( ($annot) = map { /ANNOTATION=(.*?);FUNC/ } @data );
+            $fwidth = length($fusion) if ( length($fusion) > $fwidth );
             $results{$name}->{$fusion} = [$gene,$count,$annot];
         }
     }
@@ -89,29 +97,19 @@ for my $input_file ( @files ) {
 #dd \%results;
 #exit;
 
+$fwidth += 4; # Give a little extra padding
+
 select $out_fh;
 for my $sample ( sort keys %results ) {
     printf "%-20s\n", $sample;
-    printf "\t%-34s %-8s %-8s %-8s\n", "Fusion", "Gene", "Count", "Annot";
+    printf "\t%-${fwidth}s%-15s %-8s %-8s\n", "Fusion", "Gene", "Count", "Annot";
     for my $fusion ( sort keys %{$results{$sample}} ) {
         (my $fname = $fusion) =~ s/(.*?\.(:?\S+))\..*_\d+$/$1/;
         if ( ! $novel ) {
-            next if ( grep { /NOVEL/ } @{$results{$sample}->{$fusion}} );
+            next if ( ${$results{$sample}->{$fusion}}[2] eq 'NOVEL' );
         }
-        printf "\t%-35s", $fname;
-        printf "%-8s %-8s %-8s\n", @{$results{$sample}->{$fusion}};
+        next if ( ${$results{$sample}->{$fusion}}[1] == 0 && ! $ref_calls );
+        printf "\t%-${fwidth}s", $fname;
+        printf "%-15s %-8s %-8s\n", @{$results{$sample}->{$fusion}};
     }
-    print "\n\n";
-}
-
-sub width_format {
-    my $data = shift;
-    my $width = 0;
-
-    for my $key ( %$data ) {
-        for my $fusion ( %{$$data{$key}} ) {
-            $width = length($fusion) if ( length($fusion) > $width );
-        }
-    }
-    return ( $width + 4 );
 }
