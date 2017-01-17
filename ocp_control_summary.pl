ee1874ab8eea1cb6ec3339847d0efd3c4a9d9de1
Dave Sims <misc@lamneth.net>
2017-01-17 10:23:24 -0500

    Fixed issue with no FPTP controls being used in MATCHv3.0.  Minor code cleanup

--- ocp_control_summary.pl
+++ ocp_control_summary.pl
@@ -6,19 +6,19 @@ use autodie;
 
 use Getopt::Long qw( :config bundling auto_abbrev no_ignore_case );
 use File::Basename;
-use List::Util qw(max sum);
 use Data::Dump;
 use Term::ANSIColor;
 
+my $scriptname = basename($0);
+my $version = "v2.3.0_011717-dev";
+
 # Remove when in prod.
 print "\n";
-print colored("*" x 50, 'bold yellow on_black'), "\n";
-print colored("      DEVELOPMENT VERSION OF $0\n", 'bold yellow on_black');
-print colored("*" x 50, 'bold yellow on_black');
+print colored("*" x 75, 'bold yellow on_black'), "\n";
+print colored("      DEVELOPMENT VERSION OF $scriptname\n", 'bold yellow on_black');
+print colored("*" x 75, 'bold yellow on_black');
 print "\n\n";
 
-my $scriptname = basename($0);
-my $version = "v2.2.1_011717-dev";
 my $description = <<"EOT";
 Program to pull out control data from VCF files generated from the OCP fusion pipeline on IR. Can
 report both the internal expression control data and the 5'3'Assay data.  
@@ -27,7 +27,8 @@ EOT
 my $usage = <<"EOT";
 USAGE: $scriptname [options] <vcf_file(s)>
     -F, --FP                  Output the 5'3' Assay data.
-    -m, --match_version  INT  Version of the NCI-MATCH panel to define which controls used.
+    -m, --match_version  INT  Version of the NCI-MATCH panel to define which controls used. Valid versions are
+                              1,2, or 3.
     -o, --output              Write output to file <default is STDOUT>
     -v, --version             Display version information
     -h, --help                Display this help text
@@ -76,17 +77,17 @@ if ( $outfile ) {
 }
 
 ####### ------------------------------------- END ARG PARSING ---------------------------- #######
-my (@expr_controls, @fp_controls, @used_controls);
+my (@expr_controls, @used_controls);
+my @fp_controls = qw( NTRK1_5p3p ALK_5p3p ROS1_5p3p RET_5p3p );
 my %results;
+
 for my $input_file ( @files ) {
     # Need to specify which depending on the panel run.
     @expr_controls = select_expr_controls(\$input_file, $match_version);
-dd \@expr_controls;
-exit;
     for my $control (@expr_controls) {
         push(@used_controls, $control) unless grep {$_ eq $control} @used_controls;
     }
-    @fp_controls = qw( NTRK1_5p3p ALK_5p3p ROS1_5p3p RET_5p3p );
+    
     (my $name = basename($input_file)) =~ s/\.vcf//;
     open( my $in_fh, "<", $input_file ) || die "Can't open the file '$input_file' for reading: $!";
     my %parsed_data;
@@ -111,14 +112,15 @@ exit;
         # Add in the expression control sum data
         $parsed_data{expr}->{'Total'} = $sum;
 
-        # Add in the 5P3P Data
-        if ( /5p3pAssays/ ) {
-            # TODO: 5'3' assays are being removed it looks like.
+        # Add in the 5P3P Data unless we're running version 3, which eliminated the FP controls.
+        if ( /5p3pAssays/ and $match_version != 3 ) {
             my @data = split;
             my ($gene, $count, $ratio) = map { /GENE_NAME=(.*?);READ_COUNT=(\d+,\d+);5P_3P_ASSAYS=(.*?);/ } @data;
             $parsed_data{fptp}->{"${gene}_5p3p"} = [$count,sprintf("%.4g", $ratio)];
         }
     }
+    #dd \%parsed_data;
+    #exit;
 
     # If no data collected, this might be a DNA only sample and not run through fusion pipeline
     if ( ! %parsed_data ) {
@@ -133,15 +135,17 @@ exit;
         :  ($results{$name}->{expr}{$control} = $parsed_data{expr}->{$control} );
     }
     
+    next if $match_version == 3;
     for my $control ( @fp_controls ) {
         ($parsed_data{fptp}->{$control}[0] eq '0,0')
         ?  ($results{$name}->{fptp}{$control} = [' ---', ' ---'])
         :  ($results{$name}->{fptp}{$control} = $parsed_data{fptp}->{$control} );
     }
 }
+#dd \%results;
+#exit;
 
 # Get the longest sample name width
-#my ($width) = max( map { length($_)+4 } keys %results );
 my $width = 0;
 for my $file (keys %results) {
     my $len = length($results{$file}->{sample});
@@ -184,7 +188,7 @@ for my $sample ( sort keys %results ) {
 sub select_expr_controls {
     my ($vcf,$version) = @_;
     my %expr_controls = (
-        0  => [qw( LMNA TBP MYC HMBS ITGB7 Total )],
+        1  => [qw( LMNA TBP MYC HMBS ITGB7 Total )],
         2  => [qw( LRP1 TBP MYC HMBS ITGB7 Total )],
         3  => [qw( LRP1 TBP MYC HMBS ITGB7 MRPL13 Total )],
     );
@@ -193,8 +197,7 @@ sub select_expr_controls {
     open(my $fh, "<", $$vcf);
     my ($ovat_version) = map { /^##OncomineVariantAnnotationToolVersion=\d\.(\d)\.\d+$/ } <$fh>;
     close $fh;
-    print "version: $ovat_version\n";
-    #exit;
-    #($ovat_version eq '2.0') ? return @{$expr_controls{1}} : return @{$expr_controls{2}};
+    $ovat_version = 1 if $ovat_version eq 0;
+    $match_version = $ovat_version;
     return @{$expr_controls{$ovat_version}};
 }
