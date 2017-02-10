a951cf255f97da09afe33015a1eb3c33374d3ed6
Dave Sims <misc@lamneth.net>
2017-02-10 16:32:59 -0500

    Added list of gene expression assays as a JSON file and started to code out a way to generate a report with this

---
+++ ocp_gene_expression_summary.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+# Load up a JSON of genes and categories for the RNA Fusions panel (the GeneExpression assays), along with a VCF
+# file, and do out parsing and outputting based on those data.
+# 2/9/2017 - D Sims
+###############################################################################################################
+use warnings;
+use strict;
+use autodie;
+
+use Data::Dump;
+use JSON;
+
+my $version = '0.1.0_020917';
+
+sub load_json {
+    my $json = shift;
+    my $jtext = do {
+        open( my $json_fh, "<", $$json);
+        local $\;
+        <$json_fh>;
+    };
+    my $jobj = JSON->new;
+    my $jdata = $jobj->decode($jtext);
+    return $jdata;
+}
+
+# Get JSON of genes and load up for easy lookup later
+my $genes_json = shift;
+my $genes = load_json(\$genes_json);
+# dd $genes;
+
+# load and parse the VCF
+my $vcf = shift;
+(my $sample_name = $vcf) =~ s/\.vcf//;
+die "error: you must load at least one VCF file!\n" unless $vcf;
+
+my %results;
+open(my $fh, "<", $vcf);
+while (<$fh>) {
+    my @data = split;
+    next unless /SVTYPE=GeneExpression/;
+    my ($counts) = $_ =~ /READ_COUNT=(\d+)/;
+    my @fields = split(/[-.]/,$data[2]);
+    push(@{$results{$fields[0]}}, {$data[2] => $counts});
+}
+
+# Dump it all out depending on gene.  will add gene expression vs WT vs splice later based on JSON input.
+#my $query_gene = 'EGFR';
+my @genes = qw(ALK AR BRAF BRCA1 BRCA2 CDKN2A EGFR ERBB2 MDM4 MET NTRK1 RB1);
+
+my $count = 0;
+for my $query_gene (@genes) {
+    for my $assay (@{$results{$query_gene}}) {
+        while ( my ($k,$v) = each %$assay ) {
+            #print join(": ", $k, $v ), "\n";
+            print join(",", $sample_name, $k, $v ), "\n";
+            $count += $v;
+        }
+    }
+}
+print "$sample_name,total,$count\n";
