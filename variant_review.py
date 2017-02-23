d8c4a0d38c4f169a0c5c3acc793c57b5538f1b23
Dave Sims <misc@lamneth.net>
2017-02-23 15:31:45 -0500

    Reworked script to be more flexible and pull more common params.  Streamlined some code and allowed non-me user to have more flexibility in less hardcoded params

--- variant_review.py
+++ variant_review.py
@@ -7,9 +7,9 @@ import argparse
 import subprocess
 import fnmatch
 from time import sleep
-from pprint import pprint
+from pprint import pprint as pp
 
-version = '1.6.1_121416'
+version = '2.0.0_022317'
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -24,56 +24,50 @@ def get_args():
     parser.add_argument("rna_bam", help='RNA BAM file from MATCHBox.')
     parser.add_argument('-a', '--analysis_id', metavar='<ir_analysis_id>', 
             help='The IR analysis ID that will be used to pull the data from the IR server if different than in BAM name')
-    parser.add_argument('-t', '--token', metavar='<ir_token_path>', 
-            help='The path to the Ion Reporter token file used to pull the analysis from the server')
+    parser.add_argument('-i', '--ip', metavar='<ip_address>', 
+            help='IP Address of server if not using config file (not recommended!). Must be used with the "--token" option')
+    parser.add_argument('-t', '--token', metavar='<ir_token>', 
+            help='An Ion Reporter API token to use with the "--ip" address function if not using a config file (not recommended!)')
     parser.add_argument('-s', '--site', metavar='<match_site_id>', default='nci',
             help='The site ID used in the ir_api_retrieve script to pull out the data (DEFAULT: %(default)s)')
 
-    requiredNamed = parser.add_argument_group('required named arguments')
-    requiredNamed.add_argument('-p', '--psn', metavar='<PSN>', required=True,
-            help='PSN for this case')
-    requiredNamed.add_argument('-m', '--msn', metavar='<MSN>', required=True,
-            help='MSN for this case')
+    requiredNamed = parser.add_argument_group('Required Named Arguments')
+    requiredNamed.add_argument('-p', '--psn', metavar='<PSN>', required=True, help='PSN for this case')
+    requiredNamed.add_argument('-m', '--msn', metavar='<MSN>', required=True, help='MSN for this case')
+
+    thresholdArgs = parser.add_argument_group('Variant Reporting Threholds')
+    thresholdArgs.add_argument('--cu', metavar='INT', default='4', help='CNV 5%% CI to use as lower bound for copy gain calling (DEFAULT: %(default)s)')
+    thresholdArgs.add_argument('--cl', metavar='INT', default='1', help='CNV 95%% CI to use as upper bound for copy loss calling (DEFAULT: %(default)s)')
+    thresholdArgs.add_argument('--cn', metavar='INT', help='If not useing CI data for copy number calling, the copy number over which to call amplifications. Off by default')
+    thresholdArgs.add_argument('--reads', metavar='INT', default='100', help='Number of fusion reads above which to consider a fusion positive call (DEFAULT: %(default)s)')
+    thresholdArgs.add_argument('--freq', metavar='INT', default='5', help='Threshold above which to call SNVs and Indels as a percent. (DEFAULT: %(default)s)')
+
     args = parser.parse_args()
 
-    site = args.site
-    psn  = args.psn
-    msn  = args.msn 
-
-    dna_bam = args.dna_bam
-    if not os.path.isfile(dna_bam):
-        sys.stderr.write("ERROR: '%s' does not exist!\n" % dna_bam)
+    if not os.path.isfile(args.dna_bam):
+        sys.stderr.write("ERROR: '%s' does not exist!\n" % args.dna_bam)
         sys.exit(1)
         
-    rna_bam = args.rna_bam
-    if not os.path.isfile(rna_bam):
-        sys.stderr.write("ERROR: '%s' does not exist!\n" % rna_bam)
+    if not os.path.isfile(args.rna_bam):
+        sys.stderr.write("ERROR: '%s' does not exist!\n" % args.rna_bam)
         sys.exit(1)
 
-    match=re.search('(PSN)([0-9]+$)', psn)
+    match=re.search('(PSN)([0-9]+$)', args.psn)
     if not match:
-        sys.stderr.write("ERROR: '%s' is not a valid PSN!\n" % psn)
+        sys.stderr.write("ERROR: '%s' is not a valid PSN!\n" % args.psn)
         sys.exit(1)
 
-    match=re.search('(MSN)([0-9]+)$', msn)
+    match=re.search('(MSN)([0-9]+)$', args.msn)
     if not match:
-        sys.stderr.write("ERROR: '%s' is not a valid MSN!\n" % msn)
+        sys.stderr.write("ERROR: '%s' is not a valid MSN!\n" % args.msn)
         sys.exit(1)
 
-    if args.analysis_id:
-        run_id = args.analysis_id
-    else:
-        run_id = ''
-
-    # Check for and hand the IR token file.
-    token = os.environ['HOME'] + '/Dropbox/ir_stuff/ir_tokens'
-    if args.token:
-        token = args.token
-    if not os.path.exists(token):
-        sys.stderr.write("ERROR: The IR token file '%s' can not be found! You might need to load a custom token with the '-t' option.\n" % token)
-        sys.exit(1)
-
-    return (dna_bam, rna_bam, psn, msn, site, run_id, token)
+    if args.ip:
+        args.site = None
+        if not args.token:
+            sys.stderr.write('ERROR: you must supply an API token if using the "--ip" option!\n')
+            sys.exit(1)
+    return args
 
 def gen_wd(psn,msn):
     new_dir = psn +'_'+ msn +'_variant_reports'
@@ -117,8 +111,6 @@ def user_query(query, default='no'):
 
 def validate_bams(msn, bam, na_type):
     '''Validate the BAM files passed into the script, and if OK, get a new file name, get analysis ID, and index them'''
-    # match = re.search('^.*?(MSN\d+_.*?_[cv]\d+_.*)_([dr]na).bam', bam)
-    # match = re.search('^.*?(MSN\d+_[cv]\d+_.*)_([dr]na).bam', bam)
     match = re.search('^.*?(MSN\d+_(?:[DR]NA_)?[cv]\d+_.*)_([dr]na).bam', bam)
     try:
         sample = match.group(2)
@@ -149,13 +141,21 @@ def validate_bams(msn, bam, na_type):
 
     return (new_file_name, analysis_id)
 
-def get_ir_data(analysis_id, token):
-    '''Use ir_api_retrieve and extract_ir_data to get IR data for review'''
+def gen_retr_cmd(arg_list):
+    '''Generate a option list to pass to ir api retrieve based on how we want to run.'''
+    if arg_list['host']:
+        cmd = ['ir_api_retrieve.py', arg_list['host'], arg_list['analysis_id']]
+        if arg_list['token']:
+            cmd.extend(['-t', arg_list['token']])
+        return cmd
+    else:
+        return ['ir_api_retrieve.py', '-i', arg_list['ip'], '-t', arg_list['token'], arg_list['analysis_id']]
 
-    sys.stdout.write('Getting data from IR for analysis ID {}...'.format(analysis_id))
+def get_ir_data(ir_params):
+    '''Use ir_api_retrieve and extract_ir_data to get IR data for review'''
     count = 1
     while count:
-        p = subprocess.Popen(['ir_api_retrieve.py', '-H','nci', '-t', token, analysis_id],stdout=subprocess.PIPE,stderr=subprocess.PIPE) 
+        p = subprocess.Popen(ir_params,stdout=subprocess.PIPE,stderr=subprocess.PIPE) 
         result,error = p.communicate()
         if p.returncode != 0:
             sys.stderr.write("\nError retrieving data: {}\n".format(error))
@@ -174,49 +174,75 @@ def get_ir_data(analysis_id, token):
     p.communicate()
     sys.stdout.write("Done!\n")
 
-def gen_moi_report(msn,vcf):
+def gen_moi_report(msn,vcf,thresholds):
     filename = msn + '_MATCH_MOI_Report.txt'
 
+    cmd = ['match_moi_report.pl']
+    for key in thresholds:
+        if thresholds[key]:
+            cmd.extend([key,thresholds[key]])
+    cmd.extend(['-o',filename,vcf])
+
     sys.stdout.write("Generating a MATCH MOI Report for {}...".format(msn))
-    p=subprocess.Popen(['match_moi_report.pl', '--cu', '4', '--cl', '1', '-o', filename, vcf], stdout=subprocess.PIPE)
+    p=subprocess.Popen(cmd, stdout=subprocess.PIPE)
     result,error = p.communicate()
     sys.stdout.write("Done!\n")
     sys.stdout.write(result.decode('ascii'))
 
 def main():
-    (dna_bam, rna_bam, psn, msn, site, analysis_id, token) = get_args()
-
+    args = get_args()
+    
     sys.stdout.write("Validating DNA and RNA BAM files...\n")
-    (new_dna_bam, dna_run_id) = validate_bams(msn, dna_bam, 'dna')
-    (new_rna_bam, rna_run_id) = validate_bams(msn, rna_bam, 'rna')
+    (new_dna_bam, dna_run_id) = validate_bams(args.msn, args.dna_bam, 'dna')
+    (new_rna_bam, rna_run_id) = validate_bams(args.msn, args.rna_bam, 'rna')
 
     # Generate a new working directory for our data and move the BAMs there.
-    work_dir = gen_wd(psn, msn)
+    work_dir = gen_wd(args.psn, args.msn)
     shutil.move(new_dna_bam, work_dir)
     shutil.move(new_dna_bam + '.bai', work_dir)
     shutil.move(new_rna_bam, work_dir)
     shutil.move(new_rna_bam + '.bai', work_dir)
     os.chdir(work_dir)
 
-    # get IR data
-    if not analysis_id:
+    # Determine the Run ID
+    if not args.analysis_id:
         if dna_run_id == rna_run_id:
-            analysis_id = dna_run_id
+            run_id = dna_run_id
         else:
             sys.stderr.write("ERROR: DNA and RNA Analysis IDs do not agree!\n")
             sys.stderr.write("\tDNA: %s\n\tRNA: %s" % (dna_run_id, rna_run_id))
             sys.exit(1)
-    get_ir_data(analysis_id, token)
+    else:
+        run_id = args.analysis_id 
+
+    # Generate an IR command and retrieve data.
+    ir_arg_list = {
+        'analysis_id' : run_id,
+        'host'        : args.site,
+        'ip'          : args.ip,
+        'token'       : args.token
+    }
+
+    sys.stdout.write('Getting data from IR for analysis ID {}...'.format(run_id))
+    sys.stdout.flush()
+    get_ir_data(gen_retr_cmd(ir_arg_list))
 
     # Get the VCF file for processing.
     files = os.listdir('vcfs')
     if files:
         if fnmatch.fnmatch(files[0], '*.vcf'):
             vcf = files[0]
-
+    
     # Generate a MOI report and store it.
-    gen_moi_report(msn,'vcfs/' + vcf)
+    moi_report_params = {
+        '--cu'    : args.cu,
+        '--cl'    : args.cl,
+        '--cn'    : args.cn,
+        '--reads' : args.reads,
+        '--freq'  : args.freq
+    }
 
+    gen_moi_report(args.msn,'vcfs/' + vcf,moi_report_params)
     sys.stdout.write('\nReport generation complete.  Results can be found in {}.\n'.format(os.getcwd()))
 
 if __name__ == '__main__':
